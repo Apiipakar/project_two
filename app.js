a970824952ea52f3779e4e423412f656011d191f
Apiipakar <cayuuni37@gmail.com>
2022-12-02 01:53:03 -0800

    first_commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const decreaseBTN = document.querySelector("#decreaseBTN");
+const ResetBTN = document.querySelector("#ResetBTN");
+const increaseBTN = document.querySelector("#increaseBTN");
+const Number = document.querySelector("#Number");
+const BTn = document.querySelectorAll(".BTn");
+let count = 0;
+
+BTn.forEach((btn) => {
+  console.log(btn);
+  btn.addEventListener("click", (event) => {
+    let chosenBTn = event.currentTarget.classList;
+    if (chosenBTn.contains("decrease")) {
+      count--;
+      Number.style.color = "red";
+    } else if (chosenBTn.contains("increase")) {
+      count++;
+      Number.style.color = "green";
+    } else {
+      count = 0;
+      Number.style.color = "black";
+    }
+    Number.textContent = count;
+  });
+});
+// ==============================================================
+// --------------Second page script starts here-------------
+const facebookBTn = document.querySelector("#facebook_link");
+const tiktokBTn = document.querySelector("#Tiktok_link");
+const watsappBTn = document.querySelector("#Watsapp_link");
+
+facebookBTn.addEventListener("click", () => {
+  location.href = "https://www.facebook.com/profile.php?id=100049157563724";
+});
+
+tiktokBTn.addEventListener("click", () => {
+  location.href = "https://www.tiktok.com/@_apii_";
+});
+
+watsappBTn.addEventListener("click", () => {
+  location.href = "https://wa.me/252907006342";
+});
